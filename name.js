69646c12d04b906cb6dbf38c370a3f9bc8fc5e1c
Pie#9196 <steven.durey24@gmail.com>
2017-08-29 21:55:57 +0930

    Update name.js

--- name.js
+++ name.js
@@ -8,6 +8,26 @@
             'slow');
     });
 
+ var darkEnabled = false; 
+$(document).ready(function() {
+        $("#dark").on("click", switchDarkMode);
+        $("#reset").on("click", reset);
+        }
+      );
+      
+      function switchDarkMode(){
+        darkEnabled = !darkEnabled;
+        if(darkEnabled){
+          $("body").addClass("darkmode");
+        } else {
+          $("body").removeClass("darkmode");
+        }
+      }
+      
+      function reset(){
+        $("body").removeClass("darkmode");
+      }
+
     document.addEventListener("DOMContentLoaded", function(){
     	$('.preloader-background').delay(1700).fadeOut('slow');
 
